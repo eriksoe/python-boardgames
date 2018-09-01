be57def872430c31715412918fbd2b5d30c56772
Erik Søe Sørensen <eriksoe@gmail.com>
2018-09-01 13:21:50 +0200

    Refactor - introduce class GuiBoard.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 from appJar.appjar import gui
 
-SQR_SIZE = 40
-
 app = None
 def main():
     global app
     app = gui("ChromoDynamics", "600x400")
-    createMainWindow(app)
-    drawBoard(app)
+    board = createMainWindow(app)
+    board.redraw()
     app.go()
 
 def createMainWindow(app):
@@ -23,51 +21,60 @@ def createMainWindow(app):
     app.startLabelFrame("Board", row=0, column=1)
     app.setSticky("")
 
-    c = app.addCanvas("c")
-    app.setCanvasWidth("c", 320)
-    app.setCanvasHeight("c", 320)
-    app.setCanvasBg("c", "#999")
-    app.setCanvasRelief("c", "sunken")
-    #app.setCanvasInPadding("c", (2,5))
-    #app.setCanvasPadding("c", (4,10))
-    #app.setCanvasEvent("c", item, event, function, add=None)
-    c.bind("<Button-1>", onBoardClicked)
-    c.bind("<Double-Button-1>", onBoardDoubleClicked)
-    c.bind("<B1-Motion>", onBoardDrag)
-    c.bind("<ButtonRelease-1>", onBoardReleased)
-    c.bind("<Motion>", onBoardMotion)
+    board = GuiBoard(app)
 
     app.stopLabelFrame()
 
-    return c
+    return board
 
-def drawBoard(app):
-    c = app.getCanvas("c")
-    app.clearCanvas("c")
+class GuiBoard:
+    SQR_SIZE = 40
 
-    for y in xrange(8):
-        for x in xrange(8):
-            parity = ((x+y) & 1) > 0
-            color = "#f99" if parity else "#99f"
-            app.addCanvasRectangle("c", x*SQR_SIZE, y*SQR_SIZE, SQR_SIZE, SQR_SIZE,
-                                   width=0, fill=color)
+    def __init__(self, app):
+        c = app.addCanvas("c")
+        app.setCanvasWidth("c", 8 * GuiBoard.SQR_SIZE)
+        app.setCanvasHeight("c", 8 * GuiBoard.SQR_SIZE)
+        app.setCanvasBg("c", "#999")
+        app.setCanvasRelief("c", "sunken")
 
+        c.bind("<Button-1>", self.onBoardClicked)
+        c.bind("<Double-Button-1>", self.onBoardDoubleClicked)
+        c.bind("<B1-Motion>", self.onBoardDrag)
+        c.bind("<ButtonRelease-1>", self.onBoardReleased)
+        c.bind("<Motion>", self.onBoardMotion)
 
-def onBoardClicked(event):
-    print("onBoardClicked(%s; %s,%s)" % (event, event.x, event.y))
-    (x,y) = (event.x, event.y)
-    bx = int(x / SQR_SIZE)
-    by = int(y / SQR_SIZE)
-    print("Square clicked: %d,%d" % (bx,by))
+        self._app = app
+        self._canvas = c
 
-def onBoardDoubleClicked(event):
-    print("onBoardDoubleClicked(%d,%d)" % (event.x, event.y))
-def onBoardDrag(event):
-    print("onBoardDrag(%d,%d)" % (event.x, event.y))
-def onBoardReleased(event):
-    print("onBoardReleased(%d,%d)" % (event.x, event.y))
-def onBoardMotion(event):
-    print("onBoardMotion(%d,%d)" % (event.x, event.y))
+    def onBoardClicked(self, event):
+        print("onBoardClicked(%s; %s,%s)" % (event, event.x, event.y))
+        (x,y) = (event.x, event.y)
+        bx = int(x / GuiBoard.SQR_SIZE)
+        by = int(y / GuiBoard.SQR_SIZE)
+        print("Square clicked: %d,%d" % (bx,by))
+
+    def onBoardDoubleClicked(self, event):
+        print("onBoardDoubleClicked(%d,%d)" % (event.x, event.y))
+    def onBoardDrag(self, event):
+        print("onBoardDrag(%d,%d)" % (event.x, event.y))
+    def onBoardReleased(self, event):
+        print("onBoardReleased(%d,%d)" % (event.x, event.y))
+    def onBoardMotion(self, event):
+        print("onBoardMotion(%d,%d)" % (event.x, event.y))
+
+    def redraw(self):
+        size = GuiBoard.SQR_SIZE
+        app = self._app
+        c = self._canvas
+        app.clearCanvas("c")
+
+        for y in xrange(8):
+            for x in xrange(8):
+                parity = ((x+y) & 1) > 0
+                color = "#f99" if parity else "#99f"
+                app.addCanvasRectangle("c", x*size, y*size, size, size,
+                                       width=0, fill=color)
+
 
 
 main()
